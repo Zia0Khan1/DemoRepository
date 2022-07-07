f77b0fcc17ec2eeb6444b1b647cffd1a923d84eb
Zia0Khan1 <ziaullahk874@gmail.com>
2022-07-07 13:12:10 +0500

    adding sample file testFile.dart

---
+++ lib/App/views/testFile.dart
@@ -0,0 +1,18 @@
+//test file to check either it get uploaded or not
+
+
+import 'package:flutter/material.dart';
+
+class TestFile extends StatefulWidget {
+  const TestFile({Key? key}) : super(key: key);
+
+  @override
+  _TestFileState createState() => _TestFileState();
+}
+
+class _TestFileState extends State<TestFile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
